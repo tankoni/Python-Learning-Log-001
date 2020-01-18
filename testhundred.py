b3aa06b4559a030bac841a51a5d10029f97c93d8
tankoni <307105024@qq.com>
2020-01-18 22:30:57 +0800

    two python simple example

---
+++ testhundred.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+number = int(input("please input a number: "))
+if number <= 100:
+    print("this number is less than or equil to 100")
+else:
+    print("this number is greater than 100")
